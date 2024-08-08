d4e63599a534975f073e3e7b597b911629a94e39
kyaloeriq <kyaloeriq@gmail.com>
2024-08-08 12:44:00 -0700

    SQL script that creates a table users following these requirements

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- creates a table 'users' with specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country VARCHAR(2) NOT NULL DEFAULT 'US'
+ALTER TABLE users
+ADD CONSTRAINT country_enum CHECK (country IN ('US', 'CO', 'TN'));
