84e768c443148f1e60062fd72e3709dcb8afd720
kyaloeriq <kyaloeriq@gmail.com>
2024-08-25 10:36:55 -0700

    function that lists all documents in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -10,5 +10,7 @@ def list_all(mongo_collection: Collection) -> List[Dict]:
     """
     Lists all documents in the specified MongoDB collection.
     """
+    # Retrieve all documents from the collection using the find() method
     documents = mongo_collection.find()
+    # Convert the cursor to a list and return it, or return an empty list
     return list(documents) if documents.count() > 0 else []
