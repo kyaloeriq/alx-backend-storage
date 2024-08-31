67be31430a09f0003e25bed497c51109098c1355
kyaloeriq <kyaloeriq@gmail.com>
2024-08-31 07:26:22 -0700

    function that inserts a new document in a collection based on kwargs

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,54 +1,18 @@
 #!/usr/bin/env python3
 """
-Module that defines a function to insert a new document into a MongoDB collection.
-
-The `insert_school` function inserts a document into the provided collection based on
-the keyword arguments (`**kwargs`). It returns the `_id` of the newly inserted document.
+This module defines a function to insert a new document into a MongoDB collection.
 """
 
-from pymongo.collection import Collection
+from pymongo import collection
 from typing import Any
 
-def insert_school(mongo_collection: Collection, **kwargs: Any) -> str:
+def insert_school(mongo_collection: collection.Collection, **kwargs: Any) -> Any:
     """
-    Inserts a new document into the specified MongoDB collection.
-
-    This function inserts a document into the collection using the keyword arguments
-    provided. It returns the `_id` of the newly inserted document.
-
-    Args:
-        mongo_collection (Collection): The pymongo collection object. This object
-        represents the collection within a MongoDB database where the document will be inserted.
-        **kwargs: Arbitrary keyword arguments representing the fields and values of the document.
-
-    Returns:
-        str: The `_id` of the newly inserted document.
-
-    Example:
-        >>> from pymongo import MongoClient
-        >>> client = MongoClient('mongodb://localhost:27017/')
-        >>> db = client.myDatabase
-        >>> collection = db.school
-        >>> new_id = insert_school(collection, name="Holberton School", address="972 Mission Street")
-        >>> print(new_id)
+    Inserts a new document into the given MongoDB collection based on the provided keyword arguments.
+    
+    :param mongo_collection: The pymongo collection object.
+    :param kwargs: The key-value pairs to be inserted as a document.
+    :return: The _id of the newly inserted document.
     """
-    # Insert the document into the collection and return the new document's _id
     result = mongo_collection.insert_one(kwargs)
-    return str(result.inserted_id)
-
-if __name__ == "__main__":
-    from pymongo import MongoClient
-
-    def main():
-        # Establish a connection to the MongoDB server
-        client = MongoClient('mongodb://localhost:27017/')
-
-        # Select the database and collection
-        db = client.myDatabase
-        collection = db.school
-
-        # Insert a new document and print the new document's _id
-        new_id = insert_school(collection, name="Holberton School", address="972 Mission Street")
-        print(f"New document inserted with _id: {new_id}")
-
-    main()
+    return result.inserted_id
